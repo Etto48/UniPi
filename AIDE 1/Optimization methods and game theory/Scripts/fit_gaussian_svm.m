1e9b2a2a9633fd262a92b88d492b639d232066c7
Etto48 <ettore.ricci.coding@gmail.com>
2025-03-26 15:43:06 +0100

    Fixed gaussian

--- AIDE 1/Optimization methods and game theory/Scripts/fit_gaussian_svm.m
+++ AIDE 1/Optimization methods and game theory/Scripts/fit_gaussian_svm.m
@@ -33,9 +33,28 @@ function [la] = fit_gaussian_svm(A,B,C,gamma)
         [X,Y] = meshgrid(x_axis,y_axis);
         % Z holds the value of the decision function at each point in the meshgrid
         Z = zeros(size(X));
+        b_idx = -1;
+        % for i = 1:size(la,1)
+        %     if la(i) > 1e-6 && la(i) < C - 1e-6
+        %         b_idx = i;
+        %         break;
+        %     end
+        % end
+        min_dist = C/2;
+        for i = 1:size(la,1)
+            dist = abs(la(i) - C/2);
+            if dist < min_dist
+                min_dist = dist;
+                b_idx = i;
+            end
+        end
+        if b_idx == -1
+            error("Cannot find an i s.t. 0 < la(i) < C");
+        end
+        b = 1/y(b_idx) - (la'.*y')*K(:,b_idx);
         for i = 1:size(X,1)
             for j = 1:size(X,2)
-                Z(i,j) = sum(la.*(y.*exp(-gamma*((X(i,j)-T(:,1)).^2 + (Y(i,j)-T(:,2)).^2))));
+                Z(i,j) = sum(la.*y.*exp(-gamma*((X(i,j)-T(:,1)).^2 + (Y(i,j)-T(:,2)).^2))) + b;
             end
         end
         contour(X,Y,Z,[0 0],'k-');
